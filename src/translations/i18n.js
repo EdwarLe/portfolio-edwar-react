4b6aca35d9cbd319ac1f702cd18349c71bdc31d5
Edwar León <edwar.leon.m@gmail.com>
2023-09-16 11:36:46 -0500

    create files with info in en and es

---
+++ src/translations/i18n.js
@@ -0,0 +1,32 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import global__es from "./languages/global__es.json"
+import global__en from "./languages/global__en.json"
+
+// the translations
+// (tip move them in a JSON file and import them,
+// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
+const resources = {
+  en: {
+    translation: global__en
+  },
+  es: {
+    translation: global__es
+  }
+};
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
+    // if you're using a language detector, do not define the lng option
+
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  });
+
+  export default i18n;
