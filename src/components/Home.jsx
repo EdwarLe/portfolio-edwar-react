2531755abbc5dda6a96358f9995991b62f96c791
Edwar León <edwar.leon.m@gmail.com>
2023-11-08 18:22:21 -0500

    change CV pdf

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,8 +24,7 @@ const Home = ({ handleCloseContactForm, t, language }) => {
           </button>
           <a
             className="bg-slate-950 font-semibold p-2 px-4 hover:bg-green-600 hover:text-slate-950 hover:scale-105 transition-all dark:bg-red-600 flex items-center"
-            href={language?.language === "es" ? "/front-end_edwar-leon_HF.pdf" : "/front-end_edwar-leon_HF_EN.pdf"} download
-          >
+            href={language?.language === "es" ? "/[es]-full_stack-HF-edwar_leon.pdf" : "/[en]-full_stack-HF-edwar_leon.pdf"} target="_blank">
             {t("eng.cv")}
           </a>
         </section>
